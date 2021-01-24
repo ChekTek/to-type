f1bbbdd7ca4eb56d0bed1605de6064d4e92287ea
chektek <zhoherchak@gmail.com>
2021-01-24 18:11:12 -0500

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>To Type</h1>
+  <TextBox v-model:text="inputText"/>
+  <TextBox v-model:text="outputText"/>
+
+  <h2>Test</h2>
+  {{inputText}}
+  <hr>
+  {{outputText}}
+</template>
+
+<script>
+import TextBox from './components/TextBox.vue'
+
+export default {
+  name: 'App',
+  data: ()=> ({
+    inputText: 'Input',
+    outputText: 'Output'
+  }),
+  components: {
+    TextBox
+  }
+}
+</script>
---
+++ src/components/TextBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <textarea :value="text" @input="event => $emit('update:text', event.target.value)"></textarea>
+</template>
+
+<script>
+export default {
+  name: 'TextBox',
+  props: {
+    text: String
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
